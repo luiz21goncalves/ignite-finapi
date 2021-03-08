967f2f90088ccb32b502aff1f953f6764ca51e37
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-03-08 20:51:41 -0300

    chore: create account

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 const express = require('express')
+const { v4: uuid } = require('uuid')
 
 const app = express()
 
 app.use(express.json())
+
+const customers = []
+
+app.post('/account', (request, response) => {
+  const { name, cpf } = request.body
+
+  customers.push({
+    id: uuid(),
+    name,
+    cpf
+  })
+
+  return response.status(201).send()
+})
 
 app.listen(3333, () => console.log('Server started on port 3333!'))
