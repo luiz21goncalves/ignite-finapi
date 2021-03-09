fd53a4b7f8941a41ef8be583a97e81a8a93f6c87
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-03-08 21:09:18 -0300

    chore: list statement

--- src/index.js
+++ src/index.js
@@ -21,10 +21,25 @@ app.post('/account', (request, response) => {
   customers.push({
     id: uuid(),
     name,
-    cpf
+    cpf,
+    statement: []
   })
 
   return response.status(201).send()
+})
+
+app.get('/statement/:cpf', (request, response) => {
+  const { cpf } = request.params
+
+  const customer = customers.find((findCustomer) => findCustomer.cpf === cpf)
+
+  if (!customer) {
+    return response.status(400).json({
+      error: 'Customer not found!'
+    })
+  }
+
+  return response.json(customer.statement)
 })
 
 app.listen(3333, () => console.log('Server started on port 3333!'))
